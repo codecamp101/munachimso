d5fa1a1b941405470200a8381f1a36509f693afe
codecamp101 <phildca114@gmail.com>
2025-04-17 14:55:06 +0100

    js styling

--- wordsmith.js
+++ wordsmith.js
@@ -14,3 +14,20 @@ function writeTxt () {
         count++;//count++ keeps adding 1 to value of count
     },100);
 }
+let score = '0';
+const checkBtn = document.querySelector('#checkBtn');
+checkBtn.addEventListener('click',() =>{
+console.log('I Have Just been Clicked');
+    document.querySelector('#cup > i').textContent = score;
+    score+= 5;
+})
+document.querySelector('#action').style.backgroundImage = `url('confetti.gif')`
+// STRING Methods
+// .slice(start,end) this slices a portion of the string
+// .concat(...Strings) join all chairs in a String
+// .trim() it removes spaces at the beginnning and of a String
+//.at(...index)///choses the chair at the index
+//length tells how many char in the string
+//.tolowercase()//writes all chars in lower case
+//.touppercase()//writes all chars upper case
+//.endswich()
